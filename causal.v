c0130a88e664209bec2218e6bb21bc638e24ae50
Joseph Cutler <jwcutler@wesleyan.edu>
2022-12-08 16:13:45 -0500

    fix big bbugs

--- causal.v
+++ causal.v
@@ -12,50 +12,49 @@ Definition step (m : autom) (x : A) : A * autom :=
     | Step f => f x
     end.
 
+Definition exec (m : autom) (x : A) : A :=
+    fst (step m x).
+
 Inductive lengthN : nat -> Type :=
 | Empty_lengthN : lengthN 0
-| Cons_lengthN : forall n (x : A), lengthN n -> lengthN (S n).
+| Snoc_lengthN {n} : lengthN n -> A -> lengthN (S n).
 
-(* remove the last element *)
-Definition truncate (n : nat) (l : lengthN (S n)) : lengthN n.
-Proof.
-  inversion l. apply X.
-Defined.
 
-Theorem trunc0 : truncate 0 = (fun _ => Empty_lengthN).
+(* remove the first element *)
+Definition truncate {n : nat} (l : lengthN (S n)) : lengthN n := 
+  match l in lengthN (S n) return lengthN n with
+  | Snoc_lengthN l _ => l
+  end.
+
+
+Theorem trunc_0 : forall (l : lengthN 1), truncate l = Empty_lengthN.
 Proof.
-extensionality l.
-inversion l.
-unfold truncate.
 Admitted.
 
 Theorem lengthN_eta_0 : forall (l : lengthN 0), l = Empty_lengthN.
 Proof.
 Admitted.
 
-(* remove the first element *)
-Fixpoint unshift (n : nat) (l : lengthN (S n)) : lengthN n.
+Theorem lengthN_eta_S : forall n, forall (l : lengthN (S n)), exists x , l = Snoc_lengthN (truncate l) x.
 Proof.
-  induction n.
-   - apply Empty_lengthN.
-   - inversion l. apply (Cons_lengthN n x). apply IHn. exact X.
-Qed.
+Admitted.
+
 
 Record omega_presheaf : Type := mkPresheaf {
   fam : nat -> Type;
-  trunc : forall n, fam (S n) -> fam n
+  restrict : forall n, fam (S n) -> fam n
 }.
 
-Definition lengthN_pshf : omega_presheaf := mkPresheaf lengthN truncate.
+Definition lengthN_pshf : omega_presheaf := mkPresheaf lengthN (fun _ => truncate).
 
 Record omega_presheaf_nt (P : omega_presheaf) (Q : omega_presheaf) : Type := mkNT {
   component : forall n, fam P n -> fam Q n;
-  squares : forall n, compose (component n) (trunc P n) = compose (trunc Q n) (component (S n))
+  squares : forall n, compose (component n) (restrict P n) = compose (restrict Q n) (component (S n))
 }.
 
 (* Is this the same as later???*)
 Definition shift (P : omega_presheaf) : omega_presheaf :=
-  mkPresheaf (fun n => fam P (S n)) (fun n => trunc P (S n)).
+  mkPresheaf (fun n => fam P (S n)) (fun n => restrict P (S n)).
 
 Definition shiftNT_component {P Q} (alpha : omega_presheaf_nt P Q) : forall n, fam (shift P) n -> fam (shift Q) n :=
   fun n => component P Q alpha (S n).
@@ -63,8 +62,8 @@ Definition shiftNT_component {P Q} (alpha : omega_presheaf_nt P Q) : forall n, f
 Theorem shiftNT_squares {P Q} (alpha : omega_presheaf_nt P Q) : forall n : nat,
 compose
   (shiftNT_component alpha n)
-  (trunc (shift P) n) =
-compose (trunc (shift Q) n)
+  (restrict (shift P) n) =
+compose (restrict (shift Q) n)
   (shiftNT_component alpha (S n)).
 Proof.
   simpl. unfold shiftNT_component in *. intro n. apply (squares P Q alpha (S n)).
@@ -83,12 +82,12 @@ Definition unshiftlengthNNT_component (alpha : omega_presheaf_nt (shift lengthN_
 Theorem unshiftlengthNNT_squares (alpha : omega_presheaf_nt (shift lengthN_pshf) (shift lengthN_pshf)) : forall n : nat,
 compose
   (unshiftlengthNNT_component alpha n)
-  (trunc lengthN_pshf n) =
-compose (trunc lengthN_pshf n)
+  (restrict lengthN_pshf n) =
+compose (restrict lengthN_pshf n)
   (unshiftlengthNNT_component alpha (S n)).
 Proof.
 simpl. induction n.
- - extensionality l. cbn. unfold compose. rewrite trunc0. reflexivity.
+ - extensionality l. cbn. unfold compose. rewrite trunc_0. reflexivity.
  - simpl in *. apply (squares (shift lengthN_pshf) (shift lengthN_pshf) alpha).
 Qed.
 
@@ -114,13 +113,61 @@ Definition unshiftlengthNNT (alpha : omega_presheaf_nt (shift lengthN_pshf) (shi
 
 Definition len1Proj (x : fam lengthN_pshf 1) : A.
 Proof.
-simpl in *. inversion x. apply x0.
+simpl in *. inversion x. apply X0.
 Qed.
 
 Definition headApply (alpha : omega_presheaf_nt lengthN_pshf lengthN_pshf) : A -> A :=
-  fun a => let l1 : fam lengthN_pshf 1 := Cons_lengthN 0 a Empty_lengthN in
+  fun a => let l1 : fam lengthN_pshf 1 := Snoc_lengthN Empty_lengthN a in
            let l2 : fam lengthN_pshf 1 := component _ _ alpha 1 l1 in
            len1Proj l2.
 
 CoFixpoint fromNT (alpha : omega_presheaf_nt lengthN_pshf lengthN_pshf) : autom :=
   Step (fun a => (headApply alpha a, fromNT (unshiftlengthNNT (shiftNT alpha)))).
+
+Check lengthN_rec.
+
+Fixpoint stepN {n} (m : autom) (l : lengthN n) : autom * lengthN n :=
+    match l with
+    | Empty_lengthN => (m,Empty_lengthN)
+    | Snoc_lengthN l' x => let (m',l'') := stepN m l' in
+                              let (y,m'') := step m x in
+                              (m'',Snoc_lengthN l'' y)
+    end.
+
+Definition execN {n} (m : autom) (l : lengthN n) := snd (stepN m l).
+
+Theorem exec_S : forall n m (l : lengthN n) x, exists l' y, (execN m (Snoc_lengthN l x) = Snoc_lengthN l' y) /\ (l' = execN m l).
+Proof.
+Admitted.
+
+Theorem toNT_squares (m : autom) :
+  forall n : nat,
+   compose (execN m) (restrict lengthN_pshf n) =
+   compose (restrict lengthN_pshf n) (execN m).
+Proof.
+intros.
+induction n.
+- simpl. extensionality l. unfold compose. rewrite trunc_0. simpl. rewrite trunc_0. reflexivity.
+- extensionality l. simpl in *. unfold compose in *.
+  assert (exists x , l = Snoc_lengthN (truncate l) x) by (apply lengthN_eta_S).
+  destruct H.
+  rewrite -> H.
+  cbn.
+  assert (exists l' y, (execN m (Snoc_lengthN (truncate l) x) = Snoc_lengthN l' y) /\ (l' = execN m (truncate l))) by (apply exec_S).
+  destruct H0 as [l' [y [HSnoc Hl']]].
+  rewrite -> HSnoc.
+  rewrite -> Hl'.
+  cbn.
+  reflexivity.
+Qed.
+
+Definition toNT (m : autom) : omega_presheaf_nt lengthN_pshf lengthN_pshf :=
+    mkNT lengthN_pshf lengthN_pshf (fun n => execN m) (toNT_squares m).
+
+(*
+* - Clear conception of who the audience is –– why do they care about this equivalence?
+* - Rewrite the prof in places where it's not easy to follow: naming, tactics, etc. Make it maximally understandable.
+* - Critical pass w/ alectryon for state management.
+* - Potentailly, an appenndix with thoughts on the process of turning the proof into a literate one.
+* - Ensure you have a trial reader!
+*)
